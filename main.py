9533d6ec097898db0ee70e89b68aa204194eefdb
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-04-26 16:55:29 +0200

    finished

--- main.py
+++ main.py
@@ -10,14 +10,24 @@ def preprocess(text):
 
     # Expand clitic contractions
     expanded_tokens = []
-    for token in tokens:
+    for i, token in enumerate(tokens):
         if "'" in token:
-            if token.lower() == "can't":
-                expanded_tokens.extend(['can', 'not'])
-            elif token == "n't":
-                expanded_tokens.append("not")
+            if token == "n't":
+                if tokens[i-1] == "wo":
+                    expanded_tokens.pop()
+                    expanded_tokens.append("will")
+                    expanded_tokens.append('not')
+                elif tokens[i-1] == "ca":
+                    expanded_tokens.pop()
+                    expanded_tokens.append('can')
+                    expanded_tokens.append('not')
+                else:
+                    expanded_tokens.append("not")
             elif "'s" in token:
-                expanded_tokens.append("is")
+                if tokens[i-1] == "let":
+                    expanded_tokens.append("us")
+                else:
+                    expanded_tokens.append("is")
             elif "'m" in token:
                 expanded_tokens.append("am")
             elif "'d" in token:
@@ -28,28 +38,25 @@ def preprocess(text):
                 expanded_tokens.append("are")
             elif "'ll" in token:
                 expanded_tokens.append("will")
-            elif token.lower() == "won't":
-                expanded_tokens.append(["will", "not"])
-            elif token.lower() == "let's":
-                expanded_tokens.append(["let", "us"])
+
             else:
                 expanded_tokens.append(token)
         else:
             expanded_tokens.append(token)
     # ["m", "s", "d", "ve", "re", "ll"]
     # Tokenize multiword expressions
-    mwes = ["New York", "San Francisco", "Los Angeles", "Washington D.C."]
-    final_tokens = []
-    i = 0
-    while i < len(expanded_tokens):
-        if i < len(expanded_tokens) - 1 and expanded_tokens[i] + " " + expanded_tokens[i + 1] in mwes:
-            final_tokens.append(expanded_tokens[i] + " " + expanded_tokens[i + 1])
-            i += 2
-        else:
-            final_tokens.append(expanded_tokens[i])
-            i += 1
+    # mwes = ["New York", "San Francisco", "Los Angeles", "Washington D.C."]
+    # final_tokens = []
+    # i = 0
+    # while i < len(expanded_tokens):
+    #     if i < len(expanded_tokens) - 1 and expanded_tokens[i] + " " + expanded_tokens[i + 1] in mwes:
+    #         final_tokens.append(expanded_tokens[i] + " " + expanded_tokens[i + 1])
+    #         i += 2
+    #     else:
+    #         final_tokens.append(expanded_tokens[i])
+    #         i += 1
 
-    return final_tokens
+    return expanded_tokens
 
 
 def stem(tokens):
@@ -75,7 +82,7 @@ def lemmatize(tokens):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # text = "It'll be nice if we can't go outside. Let's stay indoors and watch a movie instead."    #text = "I'm going to New York and we're staying for a week. I love San Francisco, but I've never been to Los Angeles. Have you ever visited Washington D.C.?"
-    text = "going goes playing plays played fairness "
+    text = "let's can't won't don't didn't goes playing plays played fairness "
     print(preprocess(text), "Tokens")
     print(stem(preprocess(text)), "Stemmer")
     print(lemmatize(preprocess(text)), "Lemmatizer")
